430b8b1ac43b8e177502a9fc30c17ca974b82182
Michel <chellolivycontato@gmail.com>
2026-06-25 10:08:11 -0300

    pastas

---
+++ direcionaai/App/direcionaaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  direcionaaiApp.swift
+//  direcionaai
+//
+//  Created by Michel de Oliveira Silva on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct direcionaaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
