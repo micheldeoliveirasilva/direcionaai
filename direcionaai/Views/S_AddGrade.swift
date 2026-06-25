90611abbdc0a5e794baf7fff4f9b0c09ec120c46
gabrielbelo2007 <gabrielbelo.dev@gmail.com>
2026-06-25 11:25:29 -0300

    New Views

---
+++ direcionaai/Views/S_AddGrade.swift
@@ -0,0 +1,18 @@
+//
+//  S_AddGrade.swift
+//  direcionaai
+//
+//  Created by Gabriel Azevedo Amorim Vieira Belo on 25/06/26.
+//
+
+import SwiftUI
+
+struct S_AddGrade: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    S_AddGrade()
+}
---
+++ direcionaai/Views/S_QuickTask.swift
@@ -0,0 +1,18 @@
+//
+//  S_QuickAddTask.swift
+//  direcionaai
+//
+//  Created by Gabriel Azevedo Amorim Vieira Belo on 25/06/26.
+//
+
+import SwiftUI
+
+struct S_QuickTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    S_QuickTask()
+}
