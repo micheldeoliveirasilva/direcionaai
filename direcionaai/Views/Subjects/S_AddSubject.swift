44a0c7920c26944c5fb82f1ccbf048e964cb9175
gabrielbelo2007 <gabrielbelo.dev@gmail.com>
2026-06-26 11:49:08 -0300

    Add Sujbect View File

---
+++ direcionaai/Views/Subjects/S_AddSubject.swift
@@ -0,0 +1,18 @@
+//
+//  S_AddSubject.swift
+//  direcionaai
+//
+//  Created by Gabriel Azevedo Amorim Vieira Belo on 26/06/26.
+//
+
+import SwiftUI
+
+struct S_AddSubject: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    S_AddSubject()
+}
