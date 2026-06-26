bfe2d49612f4bbb5e0ce76f6421af901d8fa6160
Michel <chellolivycontato@gmail.com>
2026-06-26 11:22:59 -0300

    create addtask page + subjectlistview

---
+++ direcionaai/Views/Components/S_AddTask.swift
@@ -0,0 +1,18 @@
+//
+//  S_AddTask.swift
+//  direcionaai
+//
+//  Created by Michel de Oliveira Silva on 26/06/26.
+//
+
+import SwiftUI
+
+struct S_AddTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    S_AddTask()
+}
