74f87dbaeab567d6c3d8c1c404ffa3158dc00ca9
gabrielbelo2007 <gabrielbelo.dev@gmail.com>
2026-06-28 13:07:22 -0300

    Feat: Add new note (medium version and large version)

--- direcionaai/Views/Tasks/TaskView.swift
+++ direcionaai/Views/Tasks/TaskView.swift
@@ -9,10 +9,48 @@
 import SwiftUI
 
 struct TaskView: View {
+    @State private var S_addTask = false;
+    @State private var currentDetent: PresentationDetent = .medium
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        NavigationStack{
+            
+            Text("Atividades")
+                .navigationTitle("Tarefas")
+                .toolbar{
+                    
+                    ToolbarItem(placement: .topBarTrailing){
+                        Button(action: {
+                            S_addTask.toggle()
+                        }) {
+                            Label("Adicionar tarefa", systemImage: "archivebox.fill")
+                        }
+                    }
+                    
+                    ToolbarItem(placement: .topBarTrailing){
+                        Button(action: {
+                            S_addTask.toggle()
+                        }) {
+                            Label("Adicionar tarefa", systemImage: "plus")
+                        }
+                    }
+                }
+                .sheet(isPresented: $S_addTask) {
+                    
+                    S_AddTask(actualDetent: $currentDetent)
+                        .presentationDetents([.medium, .large], selection: $currentDetent)
+                        .presentationDragIndicator(.visible)
+                        .onDisappear{
+                            currentDetent = .medium
+                        }
+                }
+    
+        }
+        
     }
 }
+
 
 #Preview {
     TaskView()
