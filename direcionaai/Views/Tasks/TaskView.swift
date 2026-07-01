6d3481d68976f26128bb9eab6630858b9a5dbba1
Michel <chellolivycontato@gmail.com>
2026-06-30 21:19:47 -0300

    feat: ForEach Tasks in TaskView

--- direcionaai/Views/Tasks/TaskView.swift
+++ direcionaai/Views/Tasks/TaskView.swift
@@ -13,14 +13,17 @@ struct TaskView: View {
     @State private var S_addTask = false;
     @State private var currentDetent: PresentationDetent = .medium
     
-    @Environment(\.modelContext) var modelContext
+    
+    @Query var tasks: [UserTask]
+    // busca no swiftData as tasks armazenadas no model UserTask
+
     var body: some View {
         
         NavigationStack{
             
             
-            
             ScrollView {
+                
                 VStack(alignment: .leading) {
                     Text("Para fazer")
                         .font(.title)
@@ -30,90 +33,103 @@ struct TaskView: View {
                     ScrollView(.horizontal, showsIndicators: false) {
                         
                         HStack {
-
-                        }
-                    }
-                }
-                
-                VStack(alignment: .leading) {
-                    Text("Em andamento")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .padding()
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        
-                        HStack {
                             
-                        }
-                    }
-                }
-                
-                VStack(alignment: .leading) {
-                    Text("Concluído")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .padding()
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        
-                        HStack {
                             
+                            ForEach(tasks) { task in
+                                if task.status == .toDo {
+                                    TaskDetailView(task: task)
+                                }
+                            }
+                            // percore e cria cada task exibindo dentro do molde criado em TaskDetailView e filtrando pelo status
 
+                                
+                            }
                         }
                     }
+                    
+                    VStack(alignment: .leading) {
+                        Text("Em andamento")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding()
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            
+                            HStack {
+                                
+                                ForEach(tasks) { task in
+                                    if task.status == .inProgress {
+                                        TaskDetailView(task: task)
+                                    }
+                                }
+                            }
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            Text("Concluído")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .padding()
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                
+                                HStack {ForEach(tasks) { task in
+                                    if task.status == .done {
+                                        TaskDetailView(task: task)
+                                    }
+                                }
+                                    
+                                }
+                            }
+                        }
+                        
+                    }
+                    .padding(10)
+                    .navigationTitle("Tarefas")
+                    .toolbar{
+                        
+                        ToolbarItem(placement: .topBarTrailing){
+                            NavigationLink{
+                                ArchTasksView()
+                            } label : {
+                                Image(systemName: "archivebox.fill")
+                            }
+                        }
+                        
+                        ToolbarItem(placement: .topBarTrailing){
+                            Button(action: {
+                                S_addTask.toggle()
+                            }) {
+                                Label("Adicionar tarefa", systemImage: "plus")
+                            }
+                        }
+                        
+                        ToolbarItem(placement: .topBarLeading){
+                            Button(action: {
+                                print("Visualizar lembretes")
+                            }) {
+                                Label("Lembretes", systemImage: "bell.fill")
+                            }
+                        }
+                    }
+                    .sheet(isPresented: $S_addTask) {
+                        
+                        S_AddTask(actualDetent: $currentDetent)
+                            .presentationDetents([.medium, .large], selection: $currentDetent)
+                            .presentationDragIndicator(.visible)
+                            .onDisappear{
+                                currentDetent = .medium
+                            }
+                    }
+                    
                 }
                 
             }
-            .padding(10)
-            .navigationTitle("Tarefas")
-            .toolbar{
-                
-                ToolbarItem(placement: .topBarTrailing){
-                    NavigationLink{
-                        ArchTasksView()
-                    } label : {
-                        Image(systemName: "archivebox.fill")
-                    }
-                }
-                
-                ToolbarItem(placement: .topBarTrailing){
-                    Button(action: {
-                        S_addTask.toggle()
-                    }) {
-                        Label("Adicionar tarefa", systemImage: "plus")
-                    }
-                }
-                
-                ToolbarItem(placement: .topBarLeading){
-                    Button(action: {
-                        print("Visualizar lembretes")
-                    }) {
-                        Label("Lembretes", systemImage: "bell.fill")
-                    }
-                }
-            }
-            .sheet(isPresented: $S_addTask) {
-                
-                S_AddTask(actualDetent: $currentDetent)
-                    .presentationDetents([.medium, .large], selection: $currentDetent)
-                    .presentationDragIndicator(.visible)
-                    .onDisappear{
-                        currentDetent = .medium
-                    }
-            }
-            
         }
-        
     }
-}
 
 
 #Preview {
     
-        let subject = Subject(exams: [], subjectName: "Matemática", professorName: "", professorEmail: "", subjectDescription: "", subjectDay: "", startTime: .now, endTime: .now, absences: 0)
-        
-        let task = UserTask(taskName: "Fazer lista", priority: .high, subject: subject, dateLimit: .now, notes: "", progress: "", status: .done)
-        
     TaskView()
 }
