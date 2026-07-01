647578984e577cd331b8ddaac76a199b445bf6ae
gabrielbelo2007 <gabrielbelo.dev@gmail.com>
2026-07-01 10:45:08 -0300

    Feat: Add save button and pickers (Subject and Priority)

--- direcionaai/Views/Components/S_Task.swift
+++ direcionaai/Views/Components/S_Task.swift
@@ -6,24 +6,24 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct S_Task: View {
     @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) private var modelContext
+    
     @Binding var actualDetent: PresentationDetent // Captura o tamanho da sheet
     
-    // var existTask: Task? -> Usando o Model da Task
-
+    @Query var allSubjects: [Subject] // Captura as disciplinas salvas
+    
+    var existTask: UserTask?
+    
     //MARK: Variáveis do formulário (medium)
     
     @State private var taskName = ""
-    // @State private var categoria = "Atividade" -> Trocar pela prioridade?
-    @State private var priority = "Média"
-    @State private var subject = "ADA"
+    @State private var selectedSubject: Subject?
+    @State private var selectedPriority: Priority = .medium
     @State private var dateLimit = Date()
-    // @State private var alert = false -> Faz sentido ter um botão que escolha se quer ser alertado?
-    
-    let prioritys = ["Baixa", "Média", "Alta"]
-    let subjects = ["ADA", "Machine Learning",  "Cálculo I", "Fonética e fonologia"]
     
     //MARK: Variáveis do formulário (large)
     
@@ -38,13 +38,19 @@ struct S_Task: View {
                 Section(header: Text("Criar Atividade")){
                     TextField("Nome", text: $taskName)
                     
-                    Picker("Prioridade", selection: $priority){
-                        ForEach(prioritys, id: \.self) {Text($0)}
+                    Picker("Prioridade", selection: $selectedPriority){
+                        ForEach(Priority.allCases, id: \.self){
+                            Text($0.rawValue.capitalized).tag($0)
+                        }
                     }
                     .pickerStyle(.segmented)
                     
-                    Picker("Disciplina", selection: $subject){
-                        ForEach(subjects, id: \.self) {Text($0)}
+                    Picker("Disciplina", selection: $selectedSubject){
+                        Text("Selecione uma disciplina").tag(nil as Subject?)
+                        
+                        ForEach(allSubjects) { subject in
+                            Text(subject.subjectName).tag(subject as Subject?)
+                        }
                     }
                     .pickerStyle(.menu)
                     
@@ -64,10 +70,13 @@ struct S_Task: View {
                         }
                     }
                     
-                    if true { //existTask != nil
+                    if let task = existTask {
                         
                         Button(role: .destructive){
+                            
+                            task.status = .archived
                             dismiss()
+                            
                         } label : {
                             HStack {
                                 Spacer()
@@ -78,26 +87,25 @@ struct S_Task: View {
                     }
                     
                 }
-                
             }
-//            .onAppear{
-//                if let task = existTask {
-//                    taskName = task.taskName
-//                    priority = task.priority
-//                    subject = task.subject
-//                    dateLimit = task.dateLimit
-//                    notes = task.notes
-//                }
-//            }
+            .onAppear{
+                if let task = existTask {
+                    taskName = task.taskName
+                    self.selectedPriority = task.priority
+                    self.selectedSubject = task.subject
+                    dateLimit = task.dateLimit
+                    notes = task.notes
+                }
+            }
             .toolbar {
                 
                 ToolbarItem(placement: .principal){
-                    Text("Adicionar tarefa") //existTask == nil ? "Adicionar tarefa" : "Editar Nota")
+                    Text(existTask == nil ? "Adicionar tarefa" : "Editar Nota")
                         .font(.headline)
                         .foregroundColor(.primary)
                 }
                 
-                ToolbarItem(placement: .topBarLeading) { // Não sei se precisa ter
+                ToolbarItem(placement: .topBarLeading) {
                     Button(action: {
                         dismiss()
                     }){
@@ -107,16 +115,32 @@ struct S_Task: View {
                 
                 ToolbarItem(placement: .topBarTrailing) {
                     Button(action: {
-                        print("Alterar para salvar")
+                        
+                        if let selectedSubjectSafe = selectedSubject {
+                            
+                            if let task = existTask {
+                                
+                                task.taskName = taskName
+                                task.priority = selectedPriority
+                                task.subject = selectedSubjectSafe
+                                task.dateLimit = dateLimit
+                                task.notes = notes
+                                
+                            } else {
+                                
+                                let newTask = UserTask(title: taskName, priority: selectedPriority, subject: selectedSubjectSafe)
+                                modelContext.insert(newTask)
+                            }
+                            
+                            dismiss()
+                        }
                     }){
                         Label("Salvar", systemImage: "checkmark")
                     }
                     .disabled(taskName.isEmpty)
                 }
             }
-            
         }
-        
     }
 }
 
