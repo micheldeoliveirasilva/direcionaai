3d56f74ee9e442cd5e92d89dd644aac8cabc7378
Michel <chellolivycontato@gmail.com>
2026-07-05 10:30:44 -0300

    Fix:  DateLimit to InsertNewTask

---
+++ direcionaai/Views/Tasks/DateLimitSegmentedControl.swift
@@ -0,0 +1,18 @@
+//
+//  DateLimitSegmentedControl.swift
+//  direcionaai
+//
+//  Created by Michel de Oliveira Silva on 05/07/26.
+//
+
+import SwiftUI
+
+struct DateLimitSegmentedControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DateLimitSegmentedControl()
+}
